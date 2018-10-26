f27ff9f701513dac6f09bfab136c8d4c5d02618d
Manasa Vesala <vesalamanasa@gmail.com>
2018-10-26 00:18:44 -0700

    added remaining code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,60 @@
+// input: bun man nun sun
+// b u n
+// m a n
+// n u n
+// s u n
+
 function crypto(inputText){
   var letter = /^[a-zA-Z]+$/;
   let sentenceOne = inputText.toLowerCase().split("");
+  console.log('sentenceOne (after split) = ', sentenceOne);
   var array=[];
-  for(var i = 0 ; i <sentenceOne.length ;i ++){
+  for(var i = 0 ; i < sentenceOne.length; i++){
     if(sentenceOne[i].match(letter)){
       array.push(sentenceOne[i]);
     }
   }
-  var lengthOf = array.length;
-  var num = Math.sqrt(lengthOf);
-  var finalSentence = array.join("");
-  // var mappedSentence = array.map(function(char) {
-  for(var i=1; i <num+1 ; i ++){
-    this["finalSentence"+i] = finalSentence.slice(0,num);
-    finalSentence = finalSentence.slice(num);
-  }
-  for(var i=1; i<num ; i++){
-    this["finalArray"+i] = finalSentence1[i]+finalSentence2[i]+finalSentence3[i];
-  }
-  finalArray = finalArray1+finalArray2;
 
-  return finalArray;
+  // array = ['b', 'u', 'n', 'm', 'a', ..,]
+  // finalSentence = 'bunmannunsun'
+  var rows = Math.ceil(Math.sqrt(array.length));
+  var cols = Math.ceil(array.length/rows);
+  var finalSentence = array.join("");
+  console.log('stripped sentence - ', finalSentence);
+
+  for(var i=1; i < rows+1 ; i ++){
+    this["row"+i] = finalSentence.slice(0,cols);
+    finalSentence = finalSentence.slice(cols);
+    console.log("row"+i+" = ", this["row"+i]);
+  }
+
+  for(var i = 1; i < cols+1; i++){
+    this["cols"+i] = "";
+  }
+
+  for(var i = 0; i < cols; i++) {
+    let colName = "cols"+ (i + 1);
+    for(var j = 1; j < rows+1; j++){
+      this[colName] = this[colName] + this["row"+j][i];
+    }
+
+    console.log(`${colName} = ${this[colName]}`);
+  }
+  var colsJoin = "";
+  for(var i = 1; i < cols+1; i++){
+    colsJoin = colsJoin + this["cols"+i];
+  }
+
+  // nn
+  // bmnsu; bmnsu
+  // uuunn; bmnsu uuunn nn
+  var output = "";
+  while(colsJoin.length > 0){
+    output =  output + colsJoin.slice(0,5) + " ";
+    colsJoin= colsJoin.slice(5);
+    console.log(`output: ${output}`);
+  }
+  return output;
   }
 
 
